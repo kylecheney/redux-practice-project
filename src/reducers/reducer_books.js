59983708450a8a8ee7d62687be00ef74462d6bd5
kylecheney <kylejosephcheney@gmail.com>
2017-01-19 19:18:36 -0700

    finished most of the book app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Mistborn', pages: 203 },
+    { title: 'Harry Potter', pages: 300 },
+    { title: 'The Walking Drum', pages: 150 },
+    { title: 'Stormlight Archives', pages: 1048 }
+  ]
+}
